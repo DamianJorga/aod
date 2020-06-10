d065f303bb8d76e8f350d30a8b2e9ad954af4afd
Damian <damian.jorga@wp.pl>
2020-06-10 20:44:05 +0200

    complete styles mobile and small tablet

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -6,12 +6,12 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('sass/*.scss')
+  return gulp.src('sass/styles.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('../css'));
 });
  
 gulp.task('watch', () => {
-  gulp.watch('sass/*.scss', gulp.series('sass'))
+  gulp.watch('sass/**/*.scss', gulp.series('sass'))
 });
 //task to minify js to /js 
